9a11ad0b4e99fd1fdbd651e99efc950783486f3e
jcampos <jcampos.jc38@gmail.com>
2021-03-11 01:54:20 -0600

    Eureka config for all clients

--- shopping-service/src/main/java/jcampos/training/microservices/shoppingservice/ShoppingServiceApplication.java
+++ shopping-service/src/main/java/jcampos/training/microservices/shoppingservice/ShoppingServiceApplication.java
@@ -2,7 +2,9 @@ package jcampos.training.microservices.shoppingservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class ShoppingServiceApplication {
 
